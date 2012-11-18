9b4043896d8d17f6097b3b306c243b1199fb93a7
Paul Reinheimer <paul@preinheimer.com>
2012-11-18 07:03:34 -0500

    - Adding display limit, defaulting to how many rows should be displayed

--- web/app/bootstrap.php
+++ web/app/bootstrap.php
@@ -6,6 +6,7 @@ define('ROOT_DIR', dirname(__DIR__));
 define('DATE_FORMAT', 'M jS H:i:s');
 define('PIE_COUNT', 5);
 define('DETAIL_COUNT', 6);
+define('DISPLAY_LIMIT', 25);
 
 require ROOT_DIR . '/vendor/Twig/Autoloader.php';
 Twig_Autoloader::register();
