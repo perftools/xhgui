88ff53253fe3a039127bf7d93fcbd8e4d05b1fc0
Paul Reinheimer <paul@preinheimer.com>
2012-11-18 04:09:40 -0500

    - Tweaking the (still possibly wrong) stuff for determining exclusive times

--- web/app/utility.php
+++ web/app/utility.php
@@ -5,6 +5,26 @@
 // We get a==>b as the name, we need a key for a and b in the array
 // to get exclusive values for A we need to subtract the values of B (and any other children);
 //call passing in the entire profile only, should return an array of functions with their regular timing, and exclusive numbers inside ['exclusive']
+/*
+ *
+ *
+ * Consider:
+ *               ---c---d---e
+ *          a -/----b---d---e
+ *
+ * We have c==>d and b==>d, and in both instances d invokes e, yet we will
+ * have but a single d==>e result. This is a known and documented limitation of XHProf
+ *
+ * We have one d==>e entry, with some values, including ct=2
+ * We also have c==>d and b==>d
+ *
+ * We should determine how many ==>d options there are, and equally
+ * split the cost of d==>e across them since d==>e represents the sum total of all calls.
+ *
+ * Notes:
+ *  Function names are not unique, but we're merging them
+ *
+ */
 function exclusive($run)
 {
     $final = array();
@@ -22,11 +42,19 @@ function exclusive($run)
         $data['ect'] = $data['ct'];
         $data['epmu'] = $data['pmu'];
         
+        
         //Set parent
         $data['parent'] = $parent;
         
-        //Save all this data as the child function, this is wrong (since we'll clobber something if the same function is called from two places)
-        $final[$child] = $data;
+        
+        if(!isset($final[$child]))
+        {
+            //Save all this data as the child function, this is wrong (since we'll clobber something if the same function is called from two places)
+            $final[$child] = $data;    
+        }else {
+            $final[$child] = addFunctions($final[$child], $data);
+        }
+        
     }
     
     //Delete from parent its children, this is wrong
@@ -45,6 +73,16 @@ function exclusive($run)
         
     }
     return $final;
+}
+
+function addFunctions($a, $b)
+{
+    $c = array();
+    foreach($a as $k => $v)
+    {
+        $c[$k] = $v + $b[$k];
+    }
+    return $c;
 }
 
 function splitName($name)
