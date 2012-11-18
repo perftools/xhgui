c05d315c8f4c238fe9e96972a8405dc738479e08
Paul Reinheimer <paul@preinheimer.com>
2012-11-18 04:10:56 -0500

    - Adding details to a few of the blocks on the run page

--- web/webroot/run.php
+++ web/webroot/run.php
@@ -27,9 +27,35 @@ function build_sorter($key) {
 
 uasort($profile, build_sorter('ewt'));
 
+//Pie Chart Widget
+$pie = array_slice($profile, 0, PIE_COUNT);
+
+//Memory Block
+$memory = $profile;
+uasort($memory, build_sorter('emu'));
+
+
+//Watched Functions Block
+//The purpose of watched functions is to let developers call out functions whose performance they want to keep an eye on, they'll
+//  always show up in the top left.
+//Ideas: mysqli_query(), memcache_set(), etc. If those explode, it's an immediate red flag
+$watches = array();
+$watch_list = array(
+    'stuff', 'strlen'
+);
+foreach($watch_list as $watchey)
+{
+    if (isset($profile[$watchey]))
+    {
+        $watches[$watchey] = $profile[$watchey];
+    }
+}
 
 $template = load_template('runs/view.twig');
 echo $template->display(array(
     'profile' => $profile,
     'result' => $result,
+    'pie_data' => $pie,
+    'memory' => $memory,
+    'watches' => $watches
 ));
