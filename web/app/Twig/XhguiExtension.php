48c92a594b2620a8bc1aab519f20f42007b546f6
Mark Story <mark@mark-story.com>
2012-12-06 22:51:42 -0500

    Fix url generation.
    The old links didn't work in subdirectories.  This should make
    various install options easier down the road.

---
+++ web/app/Twig/XhguiExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+class Twig_XhguiExtension extends Twig_Extension
+{
+    public function getName()
+    {
+        return 'xhgui';
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'url' => new Twig_Function_Method($this, 'url')
+        );
+    }
+
+    protected function _getBase()
+    {
+        return dirname($_SERVER['PHP_SELF']);
+    }
+
+    /**
+     * Get a URL for xhgui.
+     *
+     * @param string $path The file/path you want a link to
+     * @param array $queryarg Additional querystring arguments.
+     * @return string url.
+     */
+    public function url($path, $queryargs = array())
+    {
+        $base = $this->_getBase();
+        $query = '';
+        if (!empty($queryargs)) {
+            $query = '?' . http_build_query($queryargs);
+        }
+        $path = '/' . ltrim($path, '/');
+        return $base . $path . $query;
+    }
+}
+
--- web/app/bootstrap.php
+++ web/app/bootstrap.php
@@ -5,15 +5,17 @@
 define('ROOT_DIR', dirname(__DIR__));
 
 require ROOT_DIR . '/vendor/Twig/Autoloader.php';
-require ROOT_DIR . '/app/utility.php';
 Twig_Autoloader::register();
 
+require ROOT_DIR . '/app/utility.php';
+require ROOT_DIR . '/app/Twig/XhguiExtension.php';
 
 function load_template($name) {
     static $environment;
     if (empty($environment)) {
         $loader = new Twig_Loader_Filesystem(ROOT_DIR . '/templates/');
         $environment = new Twig_Environment($loader);
+        $environment->addExtension(new Twig_XhguiExtension());
     }
     return $environment->loadTemplate($name);
 }
