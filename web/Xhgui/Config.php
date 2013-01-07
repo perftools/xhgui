60c2004e159a987ad474c2e237a0660b49eddb9e
Mark Story <mark@mark-story.com>
2013-01-06 21:27:06 -0500

    Add a basic config read/write class.

---
+++ web/Xhgui/Config.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Loads and reads config file.
+ */
+class Xhgui_Config
+{
+    static $_config = array();
+
+    /**
+     * Load a config file, it will replace
+     * all the currently loaded configuration.
+     *
+     * @param string $file
+     * @return void
+     */
+    public function load($file)
+    {
+        $config = include($file);
+        self::$_config = $config;
+    }
+
+    /**
+     * Read a config value.
+     *
+     * @param string $name The name of the config variable
+     * @return The value or null.
+     */
+    public function read($name)
+    {
+        if (isset(self::$_config[$name])) {
+            return self::$_config[$name];
+        }
+        return null;
+    }
+
+    /**
+     * Write a config value.
+     *
+     * @param string $name The name of the config variable
+     * @param mixed $value The value of the config variable
+     * @return void
+     */
+    public function write($name, $value)
+    {
+        self::$_config[$name] = $value;
+    }
+
+}
