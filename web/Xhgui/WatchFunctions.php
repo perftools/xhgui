3a62bffcd4c0dfcd053f430eb88c41789eccaefa
Mark Story <mark@mark-story.com>
2013-03-20 21:43:34 -0400

    Implement saving and fetching watch functions.

--- web/Xhgui/WatchFunctions.php
+++ web/Xhgui/WatchFunctions.php
@@ -8,26 +8,46 @@ class Xhgui_WatchFunctions
         $this->_collection = $collection;
     }
 
+    /**
+     * Save a value to the collection.
+     *
+     * Will do an insert or update depending
+     * on the id field being present.
+     *
+     * @param array $data The data to save.
+     * @return boolean
+     */
     public function save($data)
     {
-    }
+        if (empty($data['name'])) {
+            return false;
+        }
+        if (empty($data['_id'])) {
+            $this->_collection->insert(
+                $data,
+                array('w' => 1)
+            );
+            return true;
+        }
 
-    public function delete($id)
-    {
+        $id = new MongoId($data['_id']);
+        unset($data['_id']);
+        $this->_collection->update(
+            array('_id' => $id),
+            $data
+        );
+        return true;
     }
 
     public function getAll()
     {
-        return array(
-            array(
-                'id' => '1',
-                'name' => 'strlen',
-            ),
-            array(
-                'id' => '1',
-                'name' => 'empty',
-            ),
-        );
+        $cursor = $this->_collection->find();
+        return iterator_to_array($cursor);
+    }
+
+    public function truncate()
+    {
+        $this->_collection->drop();
     }
 
 }
