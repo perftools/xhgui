149bdbef040b86c3005383ce6409df23e5e5484b
Mark Story <mark@mark-story.com>
2013-04-15 22:45:39 -0400

    Initial pass at implementing a very basic callgraph tree.
    Basically copied from the d3.js examples, but it is a start, the current
    code hits PHP memory limits pretty easily as json_encoding the callgraph
    can easily exhaust all the memory allocated to PHP.

---
+++ web/webroot/js/callgraph.js
@@ -0,0 +1,50 @@
+/**
+ * Generate a callgraph visualization based on the provided data.
+ */
+Xhgui.callgraph = function (container, data) {
+    var el = $(container),
+        width = el.width(),
+        height = 1024;
+
+    var cluster = d3.layout.cluster()
+        .size([height, width]);
+
+    var diagonal = d3.svg.diagonal()
+        .projection(function (d) { return [d.y, d.x]; });
+
+    var svg = d3.select(container).append('svg')
+        .attr('class', 'callgraph')
+        .attr('width', width)
+        .attr('height', height)
+            .append('g').attr('transform','translate(40,0)');
+
+    var nodes = cluster.nodes(data),
+        links = cluster.links(nodes);
+
+    var link = svg.selectAll('.link')
+        .data(links)
+        .enter().append('path')
+            .attr('class', 'link')
+            .attr('d', diagonal);
+
+    var node = svg.selectAll('.node')
+        .data(nodes)
+        .enter().append('g')
+            .attr('class', 'node')
+            .attr('transform', function (d) {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+
+    node.append('circle')
+        .attr('r', 4.5);
+
+    node.append('text')
+        .attr('dx', function (d) { return d.children ? -8 : 8; })
+        .attr('dy', 3)
+        .style('text-anchor', function (d) {
+            return d.children ? 'end' : 'start';
+        })
+        .text(function (d) {
+            return d.name;
+        });
+};
