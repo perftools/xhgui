c010e64cfdaec631020ef605170e27237e872582
Mark Story <mark@mark-story.com>
2013-04-28 23:13:11 -0400

    Get a basic force directed graph working.
    It is still really hard to read, but I think trying with tree layouts
    for the links might help.

--- web/webroot/js/callgraph.js
+++ web/webroot/js/callgraph.js
@@ -8,82 +8,53 @@
 Xhgui.callgraph = function (container, data, options) {
     var el = d3.select(container),
         width = parseInt(el.style('width'), 10),
-        height = 2000;
+        height = 1000;
 
-    var cluster = d3.layout.cluster()
-        .size([height, width - 160]);
-
-    var diagonal = d3.svg.diagonal()
-        .projection(function (d) { return [d.y, d.x]; });
+    var force = d3.layout.force()
+        .charge(function(d) {
+            return -d.value * 4;
+        })
+        .linkDistance(function(d) {
+            return d.target.value * 2;
+        })
+        .size([width, height]);
 
     var svg = d3.select(container).append('svg')
         .attr('class', 'callgraph')
         .attr('width', width)
-        .attr('height', height)
-            .append('g').attr('transform','translate(40,0)');
+        .attr('height', height);
 
-    var nodes = cluster.nodes(data),
-        links = cluster.links(nodes);
+    var nodes = force.nodes(data.nodes)
+        .links(data.links)
+        .start();
 
     var link = svg.selectAll('.link')
-        .data(links)
-        .enter().append('path')
-            .attr('class', 'link')
-            .attr('d', diagonal);
-
-    var node = svg.selectAll('.node')
-        .data(nodes)
-        .enter().append('g')
-            .attr('class', 'node')
-            .attr('transform', function (d) {
-                return "translate(" + d.y + "," + d.x + ")";
-            });
+        .data(data.links)
+        .enter().append('line')
+            .attr('class', 'link');
 
     // Color scale
     var colors = d3.scale.linear()
         .domain([0, 100])
         .range(['#ffffff', '#b63c71']);
 
-    var circle = node.append('circle')
+    var gnodes = svg.selectAll('g.node')
+        .data(data.nodes)
+        .enter().append('g')
+        .attr('class', 'node')
+        .call(force.drag);
+
+    // Append dots and text.
+    var circle = gnodes.append('circle')
+        .attr('class', 'node')
         .attr('r', function (d) {
-            return 1.2 * d.value;
+            return d.value * 0.5;
         })
         .style('fill', function (d) {
             return colors(d.value);
         });
 
-    // Set tooltips on circles.
-    Xhgui.tooltip(el, {
-        bindTo: node,
-        positioner: function (d, i) {
-            // Use the transform property to
-            // find where the node is.
-            var transform = this.getAttribute('transform');
-            var match = transform.match(/translate\((.*),(.*)\)/);
-            var xOffset = parseFloat(match[1]);
-            var yOffset = parseFloat(match[2]);
-
-            return {
-                // 7 = 1/2 width of arrow, 40 = canvas translate
-                x: xOffset + 40 - 7,
-                // 30 = fudge factor.
-                y: yOffset - 30
-            };
-        },
-        formatter: function (d, i) {
-            var urlName = '&symbol=' + encodeURIComponent(d.name);
-            var label = '<strong>' + d.name +
-                '</strong> ' + d.value + '% ' +
-                '<a href="' + options.baseUrl + urlName + '">view</a>';
-            return label;
-        }
-    });
-
-    node.append('text')
-        .attr({
-            dx: 0,
-            dy: 0
-        })
+    var text = gnodes.append('text')
         .style('display', function (d) {
             return d.value > 15 ? 'block' : 'none';
         })
@@ -95,4 +66,48 @@ Xhgui.callgraph = function (container, data, options) {
             return d.name;
         });
 
+    // Position lines / dots.
+    force.on("tick", function() {
+        link.attr("x1", function(d) {
+                return d.source.x;
+            })
+            .attr("y1", function(d) {
+                return d.source.y;
+            })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        circle.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+        text.attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; });
+    });
+
+    // Set tooltips on circles.
+    Xhgui.tooltip(el, {
+        bindTo: circle,
+        positioner: function (d, i) {
+            // Use the circle's bbox to position the tooltip.
+            var position = this.getBBox();
+
+            return {
+                // 7 = 1/2 width of arrow
+                x: position.x + (position.width / 2) - 7,
+                // 20 = fudge factor.
+                y: position.y - 20
+            };
+        },
+        formatter: function (d, i) {
+            var urlName = '&symbol=' + encodeURIComponent(d.name);
+            var label = '<strong>' + d.name +
+                '</strong> ' + d.value + '% ' +
+                '<a href="' + options.baseUrl + urlName + '">view</a>';
+            return label;
+        }
+    });
+
+    /*
+
+    */
 };
