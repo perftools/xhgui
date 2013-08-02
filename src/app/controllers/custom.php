df3cdab99a127d51a1718fb51a4c4e6edb9bfd40
Mark Story <mark@mark-story.com>
2013-08-01 23:43:08 -0400

    Move templates around for custom reports.

--- src/app/controllers/custom.php
+++ src/app/controllers/custom.php
@@ -1,14 +1,14 @@
 <?php
 
 $app->get('/custom', function () use ($app) {
-    $app->render('runs/custom_create.twig');
+    $app->render('custom/create.twig');
 })->name('custom.view');
 
 $app->get('/custom/help', function () use ($app) {
     $collection = $app->db->results;
     $res = $collection->findOne();
 
-    $app->render('runs/custom_help.twig', array(
+    $app->render('custom/help.twig', array(
         'data' => print_r($res, 1)
     ));
 })->name('custom.help');
