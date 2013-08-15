1b62f7a0792b1d297dfbfb84321185f2942128dc
Mark Story <mark@mark-story.com>
2013-08-14 22:55:37 -0400

    Update install script to check/set permissions.
    In the install script check file permissions and attempt to set g+wr.
    
    Fixes #45

--- install.php
+++ install.php
@@ -18,6 +18,9 @@ function out($out) {
     }
 }
 
+/**
+ * Composer setup.
+ */
 if (!file_exists('./composer.phar')) {
     out("Downloading composer.");
     exec("php -r \"eval('?>'.file_get_contents('https://getcomposer.org/installer'));\"", $output);
@@ -29,3 +32,25 @@ if (!file_exists('./composer.phar')) {
 out("Installing dependencies.");
 exec('php ./composer.phar update', $output);
 out($output);
+
+
+/**
+ * File permissions.
+ */
+out('Checking permissions for cache directory.');
+$worldWritable = bindec('0110000000');
+
+// Get current permissions in decimal format so we can bitmask it.
+$currentPerms = octdec(substr(sprintf('%o', fileperms('./cache')), -4));
+
+if (($currentPerms & $worldWritable) != $worldWritable) {
+	out('Attempting to set permissions on cache/');
+	$result = chmod('./cache', $currentPerms | $worldWritable);
+	if ($result) {
+		out('Permissions set on cache/');
+	} else {
+		out('Failed to set permissions on cache/ you must do it yourself.');
+	}
+} else {
+	out('Permissions on cache/ are ok.');
+}
