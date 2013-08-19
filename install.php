a9ca1a69f212632571efd764b161673a4adc87fb
Mark Story <mark@mark-story.com>
2013-08-19 11:35:04 -0400

    Use proc_open to execute shell commands.
    This will let the install script access stderr if required.
    
    Refs #50

--- install.php
+++ install.php
@@ -18,19 +18,51 @@ function out($out) {
     }
 }
 
+function runProcess($cmd, $input = null) {
+    $descriptorSpec = array(
+        0 => array('pipe', 'r'),
+        1 => array('pipe', 'w'),
+        2 => array('pipe', 'w')
+    );
+    $process = proc_open(
+        $cmd,
+        $descriptorSpec,
+        $pipes
+    );
+    if (!is_resource($process)) {
+        return 'ERROR - Could not start subprocess.';
+    }
+    $output = $error = '';
+    fwrite($pipes[0], $input);
+    fclose($pipes[0]);
+
+    $output = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+
+    $error = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+    proc_close($process);
+    if (strlen($error)) {
+        return 'ERROR - ' . $error;
+    }
+    return $output;
+}
+
 /**
  * Composer setup.
  */
 if (!file_exists('./composer.phar')) {
     out("Downloading composer.");
-    exec("php -r \"eval('?>'.file_get_contents('https://getcomposer.org/installer'));\"", $output);
+    $cmd = "php -r \"eval('?>'.file_get_contents('https://getcomposer.org/installer'));\"";
+    $output = runProcess($cmd);
     out($output);
 } else {
     out("Composer already installed.");
 }
 
 out("Installing dependencies.");
-exec('php ./composer.phar update', $output);
+$cmd = 'php ./composer.phar update --prefer-dist';
+$output = runProcess($cmd);
 out($output);
 
 
