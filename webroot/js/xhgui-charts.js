7e631c29d50c9f1b3e35442678c9e689b2605569
Bertrand Malet <malet.bertrand@gmail.com>
2013-09-22 18:24:19 +0200

    Making tooltip more consistant

--- webroot/js/xhgui-charts.js
+++ webroot/js/xhgui-charts.js
@@ -32,69 +32,14 @@ Xhgui.formatDate = d3.time.format('%Y-%m-%d');
  * @param Number decimalPlaces Number of decimal places to use.
  * @return String Formatted number string.
  */
-Xhgui.formatNumber = function (num, decimalPlaces) {
-    if (decimalPlaces === undefined) {
-        decimalPlaces = 2;
-    }
-    var sep = ',';
+Xhgui.formatNumber = d3.format('n');
 
-    number = +num;
-    var val = number.toFixed(decimalPlaces);
-    if (val < 1000) {
-        return val;
-    }
-    var split = val.split(/\./);
-    var thousands = split[0];
-    var i = thousands.length % 3 || 3;
-
-    thousands = thousands.slice(0, i) + thousands.slice(i).replace(/(\d{3})/g, sep + '$1');
-    split[0] = thousands;
-    return split.join('.');
-};
-
-
-Xhgui.legend = function(svg, text, height, margin, color) {
-    if (!text) {
-        return;
-    }
-    // position on y. 3 offsets descenders.
-    var yOffset = height + margin.bottom - 3;
-
-    // calculate the xOffset based on all the other legend.
-    // cross fingers that we don't run out of X space.
-    var legendGroups = svg.select('.legend-group');
-    var xOffset = 0;
-
-    if (legendGroups[0].length && legendGroups[0][0]) {
-        var box = legendGroups[legendGroups.length - 1][0].getBBox();
-        // 20 is some margin.
-        xOffset = box.x + box.width + 20;
-    }
-
-    var group = svg.append('g')
-        .attr('class', 'legend-group');
-
-    // Append the legend dot
-    group.append('circle')
-        .attr('fill', color)
-        .attr('r', 3)
-        .attr('cx', 0)
-        .attr('cy', -5);
-
-    // Add text.
-    group.append('text')
-        .attr('x', 5)
-        .attr('y', 0)
-        .text(text);
-
-    // position the group
-    group.attr('transform', 'translate(' + xOffset + ', ' + yOffset + ')');
-};
 
 /**
  * Bind a tooltip to an element.
  */
 Xhgui.tooltip = function (container, options) {
+
     if (
         !options.formatter ||
         !options.positioner ||
@@ -103,71 +48,18 @@ Xhgui.tooltip = function (container, options) {
         throw new Exception('You need the formatter, positioner & bindTo options.');
     }
 
-    function stop() {
-        d3.event.stopPropagation();
-    }
-
-    function createTooltip(container) {
-        var exists = container.select('.popover'),
-            popover, content;
-
-        if (exists.empty()) {
-            popover = container.append('div');
-
-            popover.attr('class', 'popover top')
-                .append('div').attr('class', 'arrow');
-
-            content = popover.append('div')
-                .attr('class', 'popover-content');
-
-            // stop flickering tooltips.
-            container.on('mouseout', stop);
-            popover.on('mouseout', stop);
-            return {frame: popover, content: content};
-        }
-        popover = exists;
-        content = exists.select('.popover-content');
-        return {frame: popover, content: content};
-    }
-
-    var tooltip = createTooltip(container);
-
-    function hide() {
-        tooltip.frame.transition().style('opacity', 0);
-        d3.select(document).on('mouseout', false);
-    }
-
     options.bindTo.on('mouseover', function (d, i) {
-        var top, left,
-            tooltipHeight, tooltipWidth,
-            content, position;
+        var content, position;
 
         // Get the tooltip content.
         content = options.formatter.call(this, d, i);
 
         // Get the tooltip position.
         position = options.positioner.call(this, d, i);
+        
+        nv.tooltip.show([position.x, position.y], content, null, null, this.parentNode);
 
-        tooltip.content.html(content);
-        tooltip.frame.style({
-            display: 'block',
-            opacity: 1
-        });
-
-        tooltipWidth = parseInt(tooltip.frame.style('width'), 10);
-        tooltipHeight = parseInt(tooltip.frame.style('height'), 10);
-
-        // Recalculate based on width/height of tooltip.
-        // arrow is 10x10, so 7 & 5 are magic numbers
-        top = position.y - (tooltipHeight / 2) - 7;
-        left = position.x - (tooltipWidth / 2) + 5;
-
-        tooltip.frame.style({
-            top: top + 'px',
-            left: left + 'px'
-        });
-
-        d3.select(document).on('mouseout', hide);
+        d3.select(document).on('mouseout', nv.tooltip.cleanup);
     });
 };
 
@@ -202,7 +94,14 @@ Xhgui.columnchart = function (container, data, options) {
           .color([])
 
       chart.yAxis
-        .tickFormat(d3.format('s'));
+        .tickFormat(d3.format('s'))
+        .showMaxMin(false);
+
+      chart.tooltipContent(function(key, x, y, e, graph) {
+            var value = e.series.values[e.pointIndex].value;
+            return '<div class="top"><strong>'+x+'</strong>'
+                   +'<br />'+Xhgui.formatNumber(value)+' '+options.postfix+'</div>';
+      });
 
       d3.select(container).append('svg')
         .attr("width", width + margin.left + margin.right)
@@ -249,7 +148,6 @@ Xhgui.linegraph = function (container, data, options) {
     
     nv.addGraph(function() {
         var chart = nv.models.lineChart();
-        
         chart.x(function(d,i) { return d[0]; })
             .y(function(d) { return d[1]; });
         chart.xAxis
@@ -260,7 +158,12 @@ Xhgui.linegraph = function (container, data, options) {
             .tickFormat(d3.format('s'))
             .showMaxMin(false)
             .axisLabel(options.postfix);
-
+        
+        chart.tooltipContent(function(key, x, y, e, graph) { 
+            var value = e.series.values[e.pointIndex][1];
+            return '<div class="top"><strong>'+x+'</strong>'
+                   +'<br />'+Xhgui.formatNumber(value)+' '+options.postfix+'</div>';
+        });
         container.append('svg')
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
