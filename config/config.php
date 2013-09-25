8453169af739222c01cc257cefac714e42f25984
Mark Story <markstory@freshbooks.com>
2013-09-25 10:15:25 -0400

    Add empty array for db options.

--- config/config.php
+++ config/config.php
@@ -7,6 +7,8 @@ return array(
     'mode' => 'development',
     'db.host' => 'mongodb://localhost:27017',
     'db.db' => 'xhprof',
+    // Allows you to pass additional options like replicaSet to MongoClient.
+    'db.options' => array(),
     'templates.path' => XHGUI_ROOT_DIR . '/src/templates',
     'date.format' => 'M jS H:i:s',
     'detail.count' => 6,
