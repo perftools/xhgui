ff6710ff3924be412d5186ab3aab36ce3b70797e
Mark Story <markstory@freshbooks.com>
2013-11-06 16:45:19 -0500

    Remove the autoloader.
    composer handles this for us as part of the installation script.
    
    Fixes #71

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,6 +5,4 @@
 define('XHGUI_ROOT_DIR', dirname(__DIR__));
 require XHGUI_ROOT_DIR . '/vendor/autoload.php';
 
-Xhgui_Autoload::register();
-
 Xhgui_Config::load(XHGUI_ROOT_DIR . '/config/config.php');
