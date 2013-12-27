c81a5904f7f7cd47819e55fd55bff45b39f99618
Mark Story <mark@mark-story.com>
2013-12-26 22:54:05 -0500

    Use the host app's autoload.php if available.
    You can now more easily install xhgui through composer as it is listed
    on packagist.org. Search for the host application's autoload.php file if
    we cannot find a more local one.
    
    Closes #78

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,12 @@
  * Boostrapping and common utility definition.
  */
 define('XHGUI_ROOT_DIR', dirname(__DIR__));
-require XHGUI_ROOT_DIR . '/vendor/autoload.php';
+
+if (file_exists(XHGUI_ROOT_DIR . '/vendor/autoload.php')) {
+    require XHGUI_ROOT_DIR . '/vendor/autoload.php';
+} elseif (file_exists(XHGUI_ROOT_DIR . '/../../autoload.php')) {
+    require XHGUI_ROOT_DIR . '/../../autoload.php';
+}
 
 Xhgui_Config::load(XHGUI_ROOT_DIR . '/config/config.default.php');
 Xhgui_Config::load(XHGUI_ROOT_DIR . '/config/config.php');
