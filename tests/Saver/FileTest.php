ae0e09bcc11ff51edb1cbf11ee0fbee1e3c8e808
Sebastian Brandt <sebbrandt87+git@gmail.com>
2014-08-01 07:58:05 +0200

    Update FileTest.php

--- tests/Saver/FileTest.php
+++ tests/Saver/FileTest.php
@@ -6,12 +6,12 @@ class Saver_FileTest extends PHPUnit_Framework_TestCase
     {
         $data = file_get_contents('tests/fixtures/results.json');
         
-        $file = XHGUI_ROOT_DIR . '/cache/' . md5(rand(0,100));
+        $file = md5(rand(0,100));
         
         $saver = new Xhgui_Saver_File($file);
         $saver->save($data);
         
-        $this->assertEquals(json_encode($data).PHP_EOL, file_get_contents($file));
+        $this->assertEquals(json_encode($data).PHP_EOL, file_get_contents(XHGUI_ROOT_DIR . '/cache/' . $file));
     }
 
 }
