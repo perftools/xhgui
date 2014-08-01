d1b6bd5a83d5d5517774661aaaef0755353e1d6e
Sebastian Brandt <sebbrandt87+git@gmail.com>
2014-08-01 07:52:57 +0200

    Update FileTest.php
    modified test to work again with the real cache folder

--- tests/Saver/FileTest.php
+++ tests/Saver/FileTest.php
@@ -6,7 +6,7 @@ class Saver_FileTest extends PHPUnit_Framework_TestCase
     {
         $data = file_get_contents('tests/fixtures/results.json');
         
-        $file = tempnam(sys_get_temp_dir(), "xhgui");
+        $file = md5(rand(0,100));
         
         $saver = new Xhgui_Saver_File($file);
         $saver->save($data);
