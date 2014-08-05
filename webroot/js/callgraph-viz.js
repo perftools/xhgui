bfa438ccf2d31547bf9b2a315ed4ddd3d8969ec3
Mark Story <mark@mark-story.com>
2014-08-04 23:33:09 -0400

    Start making chart a bit easier to read.
    * Add colors & size variance based on weight of function.

--- webroot/js/callgraph-viz.js
+++ webroot/js/callgraph-viz.js
@@ -1,4 +1,23 @@
 Xhgui.callgraph = function(container, data, option) {
+    // Color scale
+    var colors = d3.scale.linear()
+        .domain([0, 100])
+        .range(['#ffe85e', '#b63c71']);
+
+    var textSize = d3.scale.linear()
+        .domain([0, 100])
+        .range([0.5, 3]);
+
+    // Generate the style props for a node and its label
+    var nodeStyle = function(node) {
+        var ratio = node.value / data.total * 100;
+        return 'fill: ' + colors(ratio) + ';'
+    };
+    var nodeLabelStyle = function(node) {
+        var ratio = node.value / data.total * 100;
+        return 'font-size: ' + textSize(ratio) + 'em;'
+    }
+
     var el = d3.select(container),
         width = parseInt(el.style('width'), 10),
         height = 1000;
@@ -13,23 +32,20 @@ Xhgui.callgraph = function(container, data, option) {
         var node = data.nodes[i];
         g.addNode(node.name, {
             label: node.name,
-            ratio: Math.ceil(node.value / data.total * 100)
+            style: nodeStyle(node),
+            labelStyle: nodeLabelStyle(node)
         });
     }
     for (i = 0, len = data.links.length; i < len; i++) {
         var edge = data.links[i];
+        var word = edge.callCount === 1 ? ' call' : ' calls';
         g.addEdge(
             null,
             edge.source,
             edge.target,
-            {label: 'Called ' + edge.callCount + ' times'}
+            {label: edge.callCount + word}
         );
     }
-
-    // Color scale
-    var colors = d3.scale.linear()
-        .domain([0, 100])
-        .range(['#ffe85e', '#b63c71']);
 
     var renderer = new dagreD3.Renderer();
 
@@ -37,11 +53,19 @@ Xhgui.callgraph = function(container, data, option) {
     renderer.drawNodes(function(graph, root) {
         var nodes = oldDraw(graph, root);
         return nodes.each(function(u) {
+            return;
+
             var dpoint = graph.node(u);
             var box = d3.select(this).select('rect');
+
+            var width = box.attr('width');
+            var height = box.attr('height');
+
+            // Color and size the box based on how heavy it was.
             box.style('fill', function (d) {
                 return colors(dpoint.ratio);
-            });
+            }).attr('width',  width * dpoint.ratio * 0.1)
+            .attr('height', height * dpoint.ratio * 0.1);
         });
     });
 
