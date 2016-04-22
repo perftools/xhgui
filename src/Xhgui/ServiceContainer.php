4a556e92303ae519af2dfb15c79df4f6d5d76cfc
Abdul.Mohsen B. A. A <admin@arabcoders.org>
2016-04-22 22:32:41 +0300

    PHP7 compatibility

--- src/Xhgui/ServiceContainer.php
+++ src/Xhgui/ServiceContainer.php
@@ -75,7 +75,7 @@ class Xhgui_ServiceContainer extends Pimple
                 $config['db.options'] = array();
             }
             $mongo = new MongoClient($config['db.host'], $config['db.options']);
-            $mongo->$config['db.db']->results->findOne();
+            $mongo->{$config['db.db']}->results->findOne();
 
             return $mongo->{$config['db.db']};
         });
