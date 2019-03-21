7875eb94c0a12d7f7d63c2447bd5026f22bca90b
Elan Ruusamäe <glen@delfi.ee>
2019-03-21 15:02:32 +0200

    create initial xhgui database with indexes

---
+++ mongo.init.d/xhgui.js
@@ -0,0 +1,5 @@
+db.results.ensureIndex( { 'meta.SERVER.REQUEST_TIME' : -1 } );
+db.results.ensureIndex( { 'profile.main().wt' : -1 } );
+db.results.ensureIndex( { 'profile.main().mu' : -1 } );
+db.results.ensureIndex( { 'profile.main().cpu' : -1 } );
+db.results.ensureIndex( { 'meta.url' : 1 } );
