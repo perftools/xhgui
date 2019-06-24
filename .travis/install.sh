4980807d371f929d19a4128698aa5320d4f48481
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2019-06-24 11:28:06 +0200

    Mongo is no longer needed to run unittests

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-if [[ "$TRAVIS_PHP_VERSION" == "7.2" ]]; then
-	# installed, but not enabled?
-	echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-else
-	pecl install mongodb || true
-fi
-
 git clone https://github.com/tideways/php-profiler-extension.git
 cd php-profiler-extension
 
