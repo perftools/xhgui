c5c93f30be57b3dac7aa5c0a355788fe6b08ef5d
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2019-07-05 18:14:26 +0200

    Url filter fix

--- src/Xhgui/Storage/File.php
+++ src/Xhgui/Storage/File.php
@@ -124,7 +124,7 @@ class Xhgui_Storage_File extends Xhgui_Storage_Abstract implements
                 }
             }
 
-            if ($filter->getUrl() && strpos($meta['url'], $filter->getUrl()) === false) {
+            if ($filter->getUrl() && strpos($meta['simple_url'], $filter->getUrl()) === false) {
                 continue;
             }
 
