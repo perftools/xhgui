41c13b9e5ff860cb257a403bc98186004a905139
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2019-07-23 21:02:22 +0200

    Assign config variable to templates

--- src/Xhgui/Controller.php
+++ src/Xhgui/Controller.php
@@ -53,20 +53,25 @@ abstract class Xhgui_Controller
      */
     public function render()
     {
-        // We want to render the specified Twig template to the output buffer.
-        // The simplest way to do that is Slim::render, but that is not allowed
-        // in middleware, because it uses Slim\View::display which prints
-        // directly to the native PHP output buffer.
-        // Doing that is problematic, because the HTTP headers set via $app->response()
-        // must be output first, which won't happen until after the middleware
-        // is completed. Output of headers and body is done by the Slim::run entry point.
-
-        // The below is copied from Slim::render (slim/slim@2.6.3).
-        // Modified to use View::fetch + Response::write, instead of View::display.
-        $this->app->view->appendData($this->_templateVars);
-
         // render body only if template is set. Useful for ajax/json response.
         if (!empty($this->_template)) {
+
+            // assign application settings to template variable named config.
+            $container = $this->app->container->all();
+            $this->_templateVars['config'] = $container['settings'];
+
+            // We want to render the specified Twig template to the output buffer.
+            // The simplest way to do that is Slim::render, but that is not allowed
+            // in middleware, because it uses Slim\View::display which prints
+            // directly to the native PHP output buffer.
+            // Doing that is problematic, because the HTTP headers set via $app->response()
+            // must be output first, which won't happen until after the middleware
+            // is completed. Output of headers and body is done by the Slim::run entry point.
+
+            // The below is copied from Slim::render (slim/slim@2.6.3).
+            // Modified to use View::fetch + Response::write, instead of View::display.
+            $this->app->view->appendData($this->_templateVars);
+
             $body = $this->app->view->fetch($this->_template);
             $this->app->response->write($body);
         }
