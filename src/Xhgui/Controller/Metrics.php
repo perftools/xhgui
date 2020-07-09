9cb8e4d5add2ee1a2e18f10b63b5394574728a13
Dave Pifke <dpifke@wikimedia.org>
2020-07-09 15:46:23 -0700

    Add Prometheus exporter
    This adds a /metrics endpoint, to export stats about the size of the
    database table.  (This is currently PDO-only.)
    
    Bug: T256039
    Change-Id: I5f83c6fe648db6065a46ef51a110fe4278bfaeaf

---
+++ src/Xhgui/Controller/Metrics.php
@@ -0,0 +1,40 @@
+<?php
+
+use Slim\Slim;
+
+class Xhgui_Controller_Metrics extends Xhgui_Controller
+{
+    /**
+     * @var Xhgui_Searcher_Interface
+     */
+    protected $searcher;
+
+    public function __construct(Slim $app, Xhgui_Searcher_Interface $searcher)
+    {
+        parent::__construct($app);
+        $this->searcher = $searcher;
+    }
+
+    public function metrics()
+    {
+        $request = $this->app->request();
+        $response = $this->app->response();
+
+        $stats = $this->searcher->stats();
+
+        $body = "# HELP xhgui_profiles_total Number of profiles collected.\n";
+        $body .= "# TYPE xhgui_profiles_total gauge\n";
+        $body .= sprintf("xhgui_profiles_total %0.1F\n\n", $stats['profiles']);
+
+        $body .= "# HELP xhgui_profile_bytes_total Size of profiles collected.\n";
+        $body .= "# TYPE xhgui_profile_bytes_total gauge\n";
+        $body .= sprintf("xhgui_profile_bytes_total %0.1F\n\n", $stats['bytes']);
+
+        $body .= "# HELP xhgui_latest_profile_seconds UNIX timestamp of most recent profile.\n";
+        $body .= "# TYPE xhgui_latest_profile_seconds gauge\n";
+        $body .= sprintf("xhgui_latest_profile_seconds %0.1F\n", $stats['latest']);
+
+        $response->body($body);
+        $response['Content-Type'] = 'text/plain; version=0.0.4';
+    }
+}
