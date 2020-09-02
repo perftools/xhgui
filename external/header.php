cf09a9ccd85df4aeccdf31c73421ca324b2f517f
Daniele Scasciafratte <mte90net@gmail.com>
2020-09-02 19:11:10 +0200

    Fix after #310
    AS it is now using that file crash because that file doesn't exists anymore

--- external/header.php
+++ external/header.php
@@ -6,4 +6,4 @@ define('XHGUI_CONFIG_DIR', dirname(__DIR__) . '/config/');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // Include collector script.
-require_once dirname(__DIR__) . '/vendor/perftools/xhgui-collector/external/header.php';
+require_once dirname(__DIR__) . '/external/header.php';
