4c7f2f33e0f565ab5b97bdf2fe581ba27e6fbe53
Elan Ruusamäe <glen@pld-linux.org>
2020-10-24 21:25:56 +0300

    Skip saving empty profilings

--- src/Xhgui/Saver/NormalizingSaver.php
+++ src/Xhgui/Saver/NormalizingSaver.php
@@ -2,6 +2,8 @@
 
 namespace XHGui\Saver;
 
+use RuntimeException;
+
 class NormalizingSaver implements SaverInterface
 {
     private $saver;
@@ -14,6 +16,12 @@ class NormalizingSaver implements SaverInterface
     public function save(array $data, string $id = null): string
     {
         foreach ($data['profile'] as $index => &$profile) {
+            // skip empty profilings
+            if (!$profile) {
+                unset($data['profile'][$index]);
+                continue;
+            }
+
             // normalize, fill all missing keys
             $profile += [
                 'ct' => 0,
@@ -22,6 +30,11 @@ class NormalizingSaver implements SaverInterface
                 'mu' => 0,
                 'pmu' => 0,
             ];
+        }
+        unset($profile);
+
+        if (!$data['profile']) {
+            throw new RuntimeException("Skipping to save empty profiling");
         }
 
         return $this->saver->save($data, $id);
