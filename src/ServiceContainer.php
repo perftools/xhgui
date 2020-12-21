72f83357d267f2598cd5133fcc69cb084ea214a4
Elan Ruusamäe <glen@pld-linux.org>
2020-12-21 18:37:24 +0200

    Remove "Xhgui" from classes path

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -55,7 +55,7 @@ class ServiceContainer extends Container
             // Configure Twig view for slim
             $view = new Twig();
 
-            $view->twigTemplateDirs = [dirname(__DIR__, 2) . '/templates'];
+            $view->twigTemplateDirs = [dirname(__DIR__) . '/templates'];
             $view->parserOptions = [
                 'charset' => 'utf-8',
                 'cache' => $cacheDir,
