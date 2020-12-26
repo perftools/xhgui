e9ac2972a30387307b37a4184937a310e350273e
Elan Ruusamäe <glen@pld-linux.org>
2020-12-26 13:28:17 +0200

    Register view in Slim container

--- src/ServiceProvider/SlimProvider.php
+++ src/ServiceProvider/SlimProvider.php
@@ -4,7 +4,9 @@ namespace XHGui\ServiceProvider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Psr\Container\ContainerInterface;
 use Slim\App;
+use Slim\Container as SlimContainer;
 use Slim\Http\Environment;
 use Slim\Http\Uri;
 use Slim\Views\Twig;
@@ -17,37 +19,39 @@ class SlimProvider implements ServiceProviderInterface
      */
     public function register(Container $c): void
     {
-        $c['view'] = static function ($app) {
-            $view = new Twig($app['app.template_dir'], [
-                'cache' => $app['app.cache_dir'],
+        $c['app'] = function ($c) {
+            if ($c['config']['timezone']) {
+                date_default_timezone_set($c['config']['timezone']);
+            }
+
+            $app = new App($c['config']);
+            $this->registerView($app->getContainer());
+
+            /*
+            $view = $c['view'];
+            */
+
+            return $app;
+        };
+    }
+
+    private function registerView(ContainerInterface $c): void
+    {
+        $c['view'] = static function (SlimContainer $app) {
+            $view = new Twig($app['template_dir'], [
+                'cache' => $app['cache_dir'],
             ]);
 
             // Instantiate and add Slim specific extension
             $router = $app->get('router');
             $uri = Uri::createFromEnvironment(new Environment($_SERVER));
             $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+            $view->addExtension(new TwigExtension($router, $uri));
 
             // set global variables to templates
-            $view['date_format'] = $app['config']['date.format'];
+            $view['date_format'] = $app['date.format'];
 
             return $view;
-        };
-
-        $c['app'] = static function ($c) {
-            if ($c['config']['timezone']) {
-                date_default_timezone_set($c['config']['timezone']);
-            }
-
-            $app = new App($c['config']);
-
-            /*
-            $view = $c['view'];
-            $view->addExtension(new TwigExtension($app));
-
-            $app->view($view);
-            */
-
-            return $app;
         };
     }
 }
