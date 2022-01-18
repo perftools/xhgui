20cd4912b63798ddc0fe983abf85c678da285184
Elan Ruusamäe <glen@pld-linux.org>
2022-01-18 12:47:27 +0200

    Cleanup error/view.twig template. No longer used

--- src/ServiceProvider/RouteProvider.php
+++ src/ServiceProvider/RouteProvider.php
@@ -19,23 +19,6 @@ class RouteProvider implements ServiceProviderInterface
 
     private function registerRoutes(Container $di, App $app): void
     {
-        /*
-        $app->error(static function (Exception $e) use ($di, $app): void {
-            // @var Twig $view
-            $view = $di['view'];
-            $view->parserOptions['cache'] = false;
-            $view->parserExtensions = [
-                new TwigExtension($app),
-            ];
-
-            $app->view($view);
-            $app->render('error/view.twig', [
-                'message' => $e->getMessage(),
-                'stack_trace' => $e->getTraceAsString(),
-            ]);
-        });
-        */
-
         /**
          * Wrap Request/Response with RequestProxuy/RequestWrapper
          */
