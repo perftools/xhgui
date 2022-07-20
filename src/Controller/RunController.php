07d56e0ecdbaa3c32e2db312ccd1a525257a187d
sunanzhi <sunanzhi@hotmail.com>
2022-07-20 16:46:01 +0800

    remove dead code

--- src/Controller/RunController.php
+++ src/Controller/RunController.php
@@ -271,10 +271,8 @@ class RunController extends AbstractController
             'base_run' => $baseRun,
             'head_run' => $headRun,
             'candidates' => $candidates,
-            // 'url_params' => $request->get(),
             'url_params' => $request->getQueryParams(),
             'comparison' => $comparison,
-            'paging' => $paging,
             'pagination' => $paging,
             'search' => [
                 'base' => $request->get('base'),
